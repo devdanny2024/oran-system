ed1987e05c81b624aa9e0586c864e2fa7ca4f1d3
devdanny <devdanny2024@gmail.com>
2026-01-06 18:45:38 +0100

    Remove class-validator from GoogleLoginDto

--- backend/src/modules/auth/dto/google-login.dto.ts
+++ backend/src/modules/auth/dto/google-login.dto.ts
@@ -1,11 +1,5 @@
-import { IsEmail, IsOptional, IsString } from 'class-validator';
-
 export class GoogleLoginDto {
-  @IsEmail()
   email!: string;
 
-  @IsOptional()
-  @IsString()
   name?: string;
 }
-
